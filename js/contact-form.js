384aa9f6b3ee525c4b451c18caf226c8127b6a40
Ronald <chiradzaronald6@gmail.com>
2024-03-23 12:27:46 +0200

    contact form

--- js/contact-form.js
+++ js/contact-form.js
@@ -1,14 +1,24 @@
 $(document).ready(function() {
     $("#submit").click(function(event) {
       event.preventDefault(); // Prevent default form submission
-  
+      
+      // message variables
       var name = $("#f-name").val();
+      var company = $("#c-name").val();
       var email = $("#email").val();
+      var contact = $("#contact").val();
       var message = $("#message-text").val();
-        console.log(name, email, message);
+      
+      function clearMessage() {
+        setTimeout(() => {
+          $("#form-message").css("display", "none")
+        }, 5000);
+      }
       // Basic validation (more can be added)
-      if (!name || !email || !message) {
-        $("#form-message").text("Please fill in all required fields").css("color", "red");
+      if (!name || !company || !email || !contact || !message) {
+        $("#form-message").text("Please fill in all required fields").css({"display": "block", "color": "red"});
+
+        clearMessage();      
         return;
       }
   
@@ -17,15 +27,23 @@ $(document).ready(function() {
         method: "POST",
         data: {
           name: name,
+          company: company,
           email: email,
+          contact: contact,
           message: message
         },
         success: function(response) {
           $("#contactForm").trigger("reset"); // Clear form after successful submission
           $("#form-message").text(response).css("color", "green");
+
+          //clear message after 5s
+          clearMessage();
         },
         error: function(jqXHR, textStatus, errorThrown) {
           $("#form-message").text("Error sending message: " + textStatus).css("color", "red");
+
+          //clear message after 5s
+          clearMessage();
         }
       });
     });
--- sendemail.php
+++ sendemail.php
@@ -1,15 +1,17 @@
 <?php
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    print("lets go1")
+    print("lets go1");
   $name = $_POST["name"];
+  $company = $_POST["company"];
   $email = $_POST["email"];
+  $contact = $_POST["contact"];
   $message = $_POST["message"];
 
   // Your recipient email address
   $recipient = "info@eazylabz.com";
   $subject = "Contact Us Form Submission - " . $name;
-  $body = "Name: " . $name . "\nEmail: " . $email . "\nMessage:\n" . $message;
+  $body = "Name: " . $name . "\nCompany: " . $company . "\nEmail: " . $email . "\nContact: " . $contact . "\nMessage:\n" . $message;
 
   $headers = "From: " . $email . "\r\n";
   $headers .= "Reply-To: " . $email . "\r\n";
@@ -22,4 +24,3 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   }
 }
 
-?>
